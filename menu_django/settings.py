4d1a8111a1990d11e721aa02ec909a6ccdc85cb4
ivek <ivekdelfin@gmail.com>
2023-02-21 11:22:49 -0600

    se cambó user por email en login admin

--- menu_django/settings.py
+++ menu_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'rest_framework',
+    'users',
 
 
 ]
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#USERS
+AUTH_USER_MODEL= 'users.User'
+
