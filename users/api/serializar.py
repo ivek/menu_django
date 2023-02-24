d9b37f9c44e88bfee90b31f1da5d419923e06006
ivek <ivekdelfin@gmail.com>
2023-02-24 13:18:54 -0600

    encriptación de password

--- users/api/serializar.py
+++ users/api/serializar.py
@@ -5,6 +5,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
         fields = ['id','username', 'first_name', 
-                    'last_name', 'password', 'is_active', 'is_staff']
+                    'last_name', 'email', 'password', 'is_active', 'is_staff']
 
 
--- users/api/view.py
+++ users/api/view.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.hashers import make_password
 
 from users.models import User
 from users.api.serializar import UserSerializer
@@ -9,4 +10,6 @@ class UserApiViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
-    
+    def create(self, request, *args, **kwargs):
+        request.data['password']= make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
